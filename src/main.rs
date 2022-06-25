3d10ae84fbc6ccc18a581aef2de21977407743b1
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2022-06-26 08:07:53 +0900

    Draw to the frame buffer

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,14 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[no_mangle]
-pub extern "sysv64" fn kernel_main () {
+pub extern "sysv64" fn kernel_main (fb_addr: *mut u8, fb_size: u64) -> ! {
+    let fb_ptr = unsafe { core::slice::from_raw_parts_mut(fb_addr, fb_size as usize) };
+    for i in 0..(fb_size/4) {
+        let base = (i*4) as usize;
+        fb_ptr[base] = 255;
+        fb_ptr[base+1] = 0;
+        fb_ptr[base+2] = 0;
+    }
     loop{
         unsafe {
             asm!("hlt");
