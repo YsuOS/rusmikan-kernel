7d085b7de70a0156c918e3b1f4e52ce573735136
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2022-07-01 14:04:23 +0900

    Update write pixel interface remove --nmagic link option

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,54 @@
 
 use core::panic::PanicInfo;
 use core::arch::asm;
+use rusmikan::FrameBufferConfig;
+use uefi::proto::console::gop::FrameBuffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
+#[derive(Copy,Clone)]
+struct Rgb {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
 #[no_mangle]
-pub extern "sysv64" fn kernel_main (fb_addr: *mut u8, fb_size: u64) -> ! {
-    let fb_ptr = unsafe { core::slice::from_raw_parts_mut(fb_addr, fb_size as usize) };
-    for i in 0..(fb_size/4) {
-        let base = (i*4) as usize;
-        fb_ptr[base] = 255;
-        fb_ptr[base+1] = 0;
-        fb_ptr[base+2] = 0;
+pub extern "sysv64" fn kernel_main (fb_config: FrameBufferConfig) -> ! {
+    let vert = fb_config.vertical_resolution;
+    let hori = fb_config.horizontal_resolution;
+    let pixels_per_scan_line = fb_config.pixels_per_scan_line;
+    let mut fb = fb_config.frame_buffer;
+
+    let rgb = Rgb {
+        r: 241,
+        g: 141,
+        b: 0,
+    };
+
+    type PixelWriter = unsafe fn(&mut FrameBuffer, usize, Rgb);
+    unsafe fn write_pixel_rgb(fb: &mut FrameBuffer, pixel_base: usize, rgb: Rgb) {
+        fb.write_value::<[u8;3]>(pixel_base, [rgb.r, rgb.g, rgb.b]);
     }
+    unsafe fn write_pixel_bgr(fb: &mut FrameBuffer, pixel_base: usize, rgb: Rgb) {
+        fb.write_value::<[u8;3]>(pixel_base, [rgb.b, rgb.g, rgb.r]);
+    }
+    let write_pixel: PixelWriter = match fb_config.pixel_format {
+        rusmikan::PixelFormat::PixelRGBResv8BitPerColor => write_pixel_rgb,
+        rusmikan::PixelFormat::PixelBGRResv8BitPerColor => write_pixel_bgr,
+    };
+
+    for y in 0..vert {
+        for x in 0..hori {
+            unsafe {
+                write_pixel(&mut fb, (x+pixels_per_scan_line*y)*4, rgb);
+            }
+        }
+    }
+
     loop{
         unsafe {
             asm!("hlt");
