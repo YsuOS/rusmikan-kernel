12b4c400650db12f2227ebc36bf8c57a8c5d11b9
Ryosuke Yasuoka <yasuoka0830@gmail.com>
2022-12-29 16:38:43 +0900

    Inplement hardware interrupt and keyboard implementation

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,15 +1,134 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use crate::println;
+use x86_64::instructions::port::Port;
+use crate::{println, print};
+use core::ptr;
+
+struct IoApic {
+    ptr: *mut IoApicMmio,
+}
+
+impl IoApic {
+    fn new(addr: u32) -> Self {
+        Self {
+            ptr: addr as *mut IoApicMmio,
+        }
+    }
+
+    unsafe fn read(&self, reg: u32) -> u32 {
+        ptr::write_volatile(&mut (*self.ptr).reg, reg);
+        ptr::read_volatile(&mut(*self.ptr).data)
+    }
+
+    unsafe fn write(&self, reg: u32, data: u32) {
+        ptr::write_volatile(&mut (*self.ptr).reg, reg);
+        ptr::write_volatile(&mut(*self.ptr).data, data);
+    }
+}
+
+// https://wiki.osdev.org/APIC#IO_APIC_Configuration
+#[repr(C)]
+struct IoApicMmio {
+    reg: u32,   // IOAPICBASE + 0x0
+    pad: [u32; 3],
+    data: u32,   // IOAPICBASE + 0x10
+}
+
+// MMIO Address
+const IOAPIC: u32 = 0xFEC00000;
+// IOREGSEL Offset
+const IOAPICID: u32 = 0x00000000;
+const IOREDTBL: u32 = 0x00000010;
+
+// IRQ
+const IRQ_OFFSET: u8 = 32;   // first 32 entries are reserved for exception by CPU
+const IRQ_KBD: u32 = 1;
+
+const REDTBL_MASKED: u32 = 0x00010000;
+
+// MMIO Address
+const LAPIC: u32 = 0xFEE00000;
+// LAPIC Register Address
+const SVR: u32 = LAPIC + 0x000000F0;
+const EOI: u32 = LAPIC + 0x000000B0;
+
+const SVR_ENABLED: u32 = 0x00000100;
+
+pub unsafe fn init() {
+        init_idt();
+        disable_pic_8259();
+        init_lapic();
+        init_io_apic();
+        x86_64::instructions::interrupts::enable();
+}
+
+pub unsafe fn init_io_apic() {
+    let ioapic = IoApic::new(IOAPIC);
+    let max_intr = ioapic.read(IOAPICID) >> 16 & 0xFF;
+
+    // Mark all interrupts edge-triggered, active high, disable, and not routed to any CPUs.
+    for i in 0..max_intr {
+        ioapic.write(IOREDTBL+2*i, REDTBL_MASKED | (IRQ_OFFSET as u32 + i));
+        ioapic.write(IOREDTBL+2*i+1, 0);
+    }
+    
+    // FIXME: no supported SMP
+    // current implementation get lapic_id from processor that the code is currently executing
+    // on (BSP)
+    let lapic_id = *(LAPIC as *mut u32) >> 24; // Get Local APIC ID
+
+    // Mark IRQ1 interrupt edge-triggered, active high, enable, and routed to the given cpunum
+    ioapic.write(IOREDTBL+2*IRQ_KBD, IRQ_OFFSET as u32 + IRQ_KBD);
+    ioapic.write(IOREDTBL+2*IRQ_KBD+1, lapic_id << 24);
+}
+
+unsafe fn init_lapic() {
+    let svr = SVR as *mut u32;
+    *svr = SVR_ENABLED | 0xFF;
+}
+
+unsafe fn disable_pic_8259() {
+    Port::new(0xa1).write(0xffu8);
+    Port::new(0x21).write(0xffu8);
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+enum InterruptIndex {
+    _Timer = IRQ_OFFSET,
+    Keyboard,
+}
 
 static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
 
-pub fn init_idt(){
-    unsafe {
-        IDT.breakpoint.set_handler_fn(breakpoint_handler);
-        IDT.load();
-    }
+unsafe fn init_idt(){
+    IDT.breakpoint.set_handler_fn(breakpoint_handler);
+    IDT[InterruptIndex::Keyboard as usize]
+        .set_handler_fn(keyboard_interrupt_handler);
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+
+    let mut kb = Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore);
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(event)) = kb.add_byte(scancode) {
+        if let Some(key) = kb.process_keyevent(event) {
+            if let DecodedKey::Unicode(character) = key {
+                print!("{}", character);
+            }
+        }
+    }
+
+    unsafe {
+        *(EOI as *mut u32) = 0;
+    }
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod graphics;
 mod ascii_font;
 mod console;
 mod pci;
-mod ps2;
+//mod ps2;
 mod interrupts;
 mod segment;
 mod paging;
@@ -19,8 +19,6 @@ use graphics::{Graphic, Rgb};
 use core::fmt::Write;
 use console::CONSOLE;
 use pci::list_pci_devices;
-use ps2::poll;
-use interrupts::init_idt;
 use mm::{BitMapMemoryManager,BITMAP_MEMORY_MANAGER};
 
 const BG_COLOR: Rgb = Rgb { r: 241, g:141, b:0 };
@@ -44,34 +42,19 @@ pub extern "sysv64" fn kernel_main_new_stack (fb_config: &FrameBufferConfig, mem
     unsafe { segment::init() };
     unsafe { BitMapMemoryManager::init(memory_map) };
     unsafe { paging::init() };
+    unsafe { interrupts::init() };
 
-
-    //graphic.write_string(0, 16, "Hello World!", Rgb {r: 0, g: 0, b: 255});
-    
-    //CONSOLE.lock().put_string(graphic, "\
-    //    line 1\nline 2\nline 3\nline 4\nline 5\nline 6\nline 7\nline 8\nline 9\nline 10\n\
-    //    line 11\nline 12\nline 13\nline 14\nline 15\nline 16\nline 17\nline 18\nline 19\nline 20\n\
-    //    line 21\nline 22\nline 23\nline 24\nline 25\nline 26\nline 27\n");
-//    print!("Hello");
-//    println!(" World!");
     println!("This is Rusmikan");
-//    println!("1 + 2 = {}", 1 + 2);
+    println!("1 + 2 = {}", 1 + 2);
+//    x86_64::instructions::interrupts::int3();
 
 //    list_pci_devices();
-
-    unsafe { let lapic_id = *(0xfee00020 as *mut u64) >> 24; // Get Local APIC ID
-        println!("{}", lapic_id);
-    }
-    unsafe { *(0xfee000b0 as *mut u64) = 0;} // EOI
-
-    poll();
-    init_idt();
-    x86_64::instructions::interrupts::int3();
-
-    let mm = memory_map.descriptors();
-    for d in mm {
-        println!("{:?}", d);
-    }
+//
+//
+//    let mm = memory_map.descriptors();
+//    for d in mm {
+//        println!("{:?}", d);
+//    }
 
     unsafe {
         let addr = BITMAP_MEMORY_MANAGER.allocate(4).unwrap();
